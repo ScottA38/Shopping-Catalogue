b4083023b2e785aff6e8ec17c9165986ae83fa57
ScottA94 <94andersonsc@googlemail.com>
2020-04-01 16:33:05 +0100

    Added .editorconfig to local environment to help automate PSR standards without a phpcs fix-hook. Duly formatted the test index.php

--- src/index.php
+++ src/index.php
@@ -3,18 +3,17 @@
 $conn = new mysqli("mysql", "admin", "pa**w0rd", "my_db");
 //check connection
 if ($conn->connect_error) {
-  die("Connected failed: " . $conn->connect_error);
+    die("Connected failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT * FROM users";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  while ($row = $result->fetch_assoc()) {
-    echo $row['firstName']." ".$row['lastName']."<br>";
-  }
+    while ($row = $result->fetch_assoc()) {
+        echo $row['firstName']." ".$row['lastName']."<br>";
+    }
 } else {
-  echo "0 results";
+    echo "0 results";
 }
 $conn->close();
-?>
