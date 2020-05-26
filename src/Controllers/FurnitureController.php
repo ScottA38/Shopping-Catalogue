de35e302e6407818ae0439589b893622b0820d01
ScottA94 <94andersonsc@googlemail.com>
2020-05-27 00:14:49 +0300

    Refactored Controller functionality to make core reside in abstract base

--- src/Controllers/FurnitureController.php
+++ src/Controllers/FurnitureController.php
@@ -7,56 +7,11 @@ namespace WebApp\Controllers;
 use Doctrine\ORM\EntityManager;
 use WebApp\Models\Furniture;
 
-class FurnitureController implements IProductController
+class FurnitureController extends ProductController
 {
-
-    protected EntityManager $em;
-
-
     public function __construct(EntityManager $em)
     {
-        $this->em = $em;
-    }
-
-    public function add($name, $price, $dimensions): string
-    {
-        $furn = new Furniture($name, $price, $dimensions);
-        $this->em->persist($furn);
-        $this->em->flush();
-        return $furn->getSku();
-    }
-
-    public function remove(string $sku): Furniture
-    {
-        $target = $this->em->find(Furniture::class, $sku);
-        $this->em->remove($target);
-        $this->em->flush();
-        return $target;
-    }
-
-    public function removeAll(): array
-    {
-        $ents = $this->em->getRepository(Furniture::class)->findAll();
-        foreach ($ents as &$record) {
-            $this->em->remove($record);
-        }
-        $this->em->flush();
-        return $ents;
-    }
-
-    public function updatePrice($sku, $price): void
-    {
-        $target = $this->em->find(Furniture::class, $sku);
-        $target->setPrice($price);
-    }
-
-    public function get(string $sku): Furniture
-    {
-        return $this->em->find(Furniture::class, $sku);
-    }
-
-    public function getAll(): array
-    {
-        return $this->em->getRepository(Furniture::class)->findAll();
+        parent::__construct($em);
+        $this->model = Furniture::class;
     }
 }
