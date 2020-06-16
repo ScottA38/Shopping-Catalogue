371b8e0cc426855d7997159cbdcb887ec68cf590
ScottA94 <94andersonsc@googlemail.com>
2020-06-16 18:04:49 +0300

    Added better sanity checking on parameters in ProductResponder::filterArray()

--- src/Responders/ProductResponder.php
+++ src/Responders/ProductResponder.php
@@ -19,7 +19,7 @@ class ProductResponder
         'price' => [FILTER_SANITIZE_NUMBER_FLOAT, ["flags" => [FILTER_FLAG_ALLOW_FRACTION]]],
         'dimensions' => [FILTER_SANITIZE_NUMBER_INT],
         'size' => [FILTER_SANITIZE_NUMBER_INT],
-        'weight' => [FILTER_SANITIZE_NUMBER_FLOAT, ["flags" => [FILTER_FLAG_ALLOW_FRACTION]]],
+        'weight' => [FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION],
     ];
 
     /**
@@ -31,14 +31,11 @@ class ProductResponder
         assert($_POST, "No post variables to process!");
 
         foreach (array_keys($_POST) as &$paramKey) {
-            assert(
-                array_key_exists($paramKey, self::$sanitisationFilters),
-                "parameter key '$paramKey' does not have a matching sanitisation filter"
-            );
-            assert(
-                array_key_exists($paramKey, self::$validationFilters),
-                "parameter key '$paramKey' does not have a matching validation filter"
-            );
+            if (!array_key_exists($paramKey, self::$sanitisationFilters)) {
+                throw new \Exception("parameter key '$paramKey' does not have a matching sanitisation filter");
+            } elseif (array_key_exists($paramKey, self::$validationFilters)) {
+                throw new \Exception("parameter key '$paramKey' does not have a matching validation filter");
+            }
             $temp = filter_input(INPUT_POST, $paramKey, ...self::$sanitisationFilters[$paramKey]);
             $temp = filter_var($temp, ...self::$validationFilters[$paramKey]);
             assert($temp, "Variable processed unsuccessfully");
